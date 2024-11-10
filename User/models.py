d85542cc88a0fe4f3f53b5d37fc8f6e325068f40
akun950lion <dangphuocloc189@gmail.com>
2024-11-10 10:23:34 +0700

    update field gender

--- User/models.py
+++ User/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 GENDER_CHOICES = [
-    ('Nam', 'Nam'),
-    ('Nữ', 'Nữ'),
-    ('Khác', 'Khác'),
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Others', 'Others'),
 ]
 
 class Info(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, blank = False, default='')
-    gender = models.CharField(choices=GENDER_CHOICES, default='Nam', max_length=4)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=6)
     age = models.IntegerField(default=0)
     occupation = models.CharField(max_length=100, blank=True, default='')
     created = models.DateTimeField(auto_now_add=True)
